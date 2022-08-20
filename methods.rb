35699ab01cacc3db66feced6d55d16333758893c
Simeon <olabs06@yahoo.com>
2022-08-20 21:14:57 +0100

    creating methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    if name.class != nil
+        puts "Hello, #{name}!"
+    end
+
+    puts "Hello, programmer!"
+end
+
+def add (num1, num2)
+    num1 + num2
+end
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+    number/2
+end
